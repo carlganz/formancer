b863f02fbc77004d4499e87a949424b4df1efb9a
carlganz <carlganz@gmail.com>
2016-12-31 09:53:05 -0800

    myInput

--- R/inputs.R
+++ R/inputs.R
@@ -12,16 +12,32 @@
 #' @return A phone input control that can be added to a UI definition.
 #' @seealso \link[shiny]{textInput}
 
-phoneInput <- function(inputId, label, value = "", width = NULL, placeholder = NULL) 
+phoneInput <- function(inputId, label, value = "", width = NULL, placeholder = NULL, ...) 
+{
+  myInput("text",inputId, label, value, width, placeholder, class = "phone_number", ...)
+}
+
+#' @export
+#' @rdname phoneInput
+zipInput <- function(inputId, label, value = "", width = NULL, placeholder = NULL, ...) 
+{
+  myInput("text",inputId, label, value, width, placeholder, maxlength = "5",
+          onkeypress='return event.charCode >= 48 && event.charCode <= 57', ...)
+}
+
+#' @export
+#' @rdname phoneInput
+myInput <- function(type, inputId, label, value = "", width = NULL, placeholder = NULL, class = "", ...) 
 {
   value <- restoreInput(id = inputId, default = value)
+  class <- paste0("form-control ",class)
   tagList(useFormancer(),
-  div(class = "form-group shiny-input-container", style = if (!is.null(width)) 
-    paste0("width: ", validateCssUnit(width), ";"), label %AND% 
-      tags$label(label, `for` = inputId), tags$input(id = inputId, 
-                                                     type = "text", class = "form-control phone_number", 
-                                                     value = value, 
-                                                     placeholder = placeholder)))
+          div(class = "form-group shiny-input-container", style = if (!is.null(width)) 
+            paste0("width: ", validateCssUnit(width), ";"), label %AND% 
+              tags$label(label, `for` = inputId), tags$input(id = inputId, 
+                                                             type = type, class = class, 
+                                                             value = value, 
+                                                             placeholder = placeholder,...)))
 }
 
 # copied from shiny
